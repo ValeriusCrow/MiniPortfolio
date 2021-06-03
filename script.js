edd9f1a011c6adf551e8e842284606c790751910
ValeriusCrow <77670739+ValeriusCrow@users.noreply.github.com>
2021-06-03 16:12:53 +0300

    Hamburger menu + mobile adaptivity (in-work)

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let btnsOpn = document.querySelectorAll(".open-window");
-let btnsCls = document.querySelectorAll(".close-window");
-let windows = document.querySelectorAll(".window");
+const btnsOpn = document.querySelectorAll(".open-window");
+const btnsCls = document.querySelectorAll(".close-window");
+const windows = document.querySelectorAll(".window");
+const menuBody = document.querySelector(".menu__body");
 
+
+//Отображение поп-ап меню при нажатии на INFO
 for (let i = 0; i < btnsOpn.length; i++) {
     btnsOpn[i].addEventListener("click", function () {
         windows[i].style.visibility = "visible";
@@ -11,11 +14,27 @@ for (let i = 0; i < btnsOpn.length; i++) {
     })
 }
 
+//Плавная прокрутка
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+        if (iconMenu.classList.contains("_active")) {
+        document.body.classList.remove("_lock");
+        iconMenu.classList.remove("_active");
+        menuBody.classList.remove("_active");
+        }
     });
 });
+
+//Меню-бургер
+const iconMenu = document.querySelector(".menu__icon");
+if (iconMenu){
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle("_lock");
+        iconMenu.classList.toggle("_active");
+        menuBody.classList.toggle("_active");
+    });
+}
